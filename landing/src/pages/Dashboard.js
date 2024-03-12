23f9efe7867ebb68338b52eefc375983a4ee74dd
vedanshi-08 <94481269+vedanshi-08@users.noreply.github.com>
2024-03-12 19:44:48 +0530

    Update Dashboard.js
    additional features extended to dashboard

--- landing/src/pages/Dashboard.js
+++ landing/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "./Dashboard.css"
 import BannerImage from "../Assets/aboutimage.jpg"
+import Footer from "../Components/Footer"
 
 function Dashboard() {
   return (
@@ -19,18 +20,34 @@ function Dashboard() {
 </nav>
 <div className='box'>
 <div className='left-panel'>
-<button className='btn btn-outline-dark'>Canteen</button>
+<Link className='btn btn-outline-dark' to="/canteen">Canteen</Link>
 <Link className='btn btn-outline-dark' to="/visitor">Visitor Registration</Link>
-<button className='btn btn-outline-dark'>Fee Payment</button>
 <Link className='btn btn-outline-dark' to="/complaints">Complain/Suggestion</Link>
+<button className='btn btn-outline-dark'>Fee Payment</button>
 </div>
 <div >
-<img src={BannerImage} alt="" width={800} height={50}/>
+<img src={BannerImage} alt="" width={800} height={50}/><br/>
+<div className='annoucement'>
+  <div className='annoucement-header'><p align="center">- ANNOUNCEMENTS -</p></div>
+  <br/><br/>
+  <ul>
+    <li><p>Students can pre-book their daily meals from the Canteen Booking option.</p></li>
+    <br/>
+    <li><p>Visitor registration should be done 24hrs prior to the arrival for each guest.</p></li>
+    <br/>
+    <li><p>Students can pre-book their daily meals from the Canteen Booking option.</p></li>
+    <br/>
+    <li><p>Students can pre-book their daily meals from the Canteen Booking option.</p></li>
+  </ul>
+  </div>
+  
+
 </div>
 </div>
 <div className="main-content">
-        
+       
       </div>
+<Footer/>
     </div>
   )
 }
